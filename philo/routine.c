c4fdcf8a008e3da840726f6049193bed246af068
Flora Cornillon <fcornill@c1r5p8.42quebec.com>
2024-10-01 13:27:10 -0400

    philo

---
+++ philo/routine.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcornill <fcornill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 13:00:38 by fcornill          #+#    #+#             */
+/*   Updated: 2024/10/01 13:26:13 by fcornill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*Philo_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	while (1)
+	{
+		printf(GREEN"Philosopher %zu is thinking\n"RST, philo->id);
+		pthread_mutex_lock(philo->left_fork);
+		pthread_mutex_lock(philo->right_fork);
+		printf(GREEN"Philosopher %zu is eating\n"RST, philo->id);
+		usleep(1000 * philo->time_to_eat);
+		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
+		printf(GREEN"Philosopher %zu is sleeping\n"RST, philo->id);
+		usleep(1000 * philo->time_to_sleep);
+	}
+	return (NULL);
+}
